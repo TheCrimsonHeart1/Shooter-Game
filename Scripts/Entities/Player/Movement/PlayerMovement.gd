4453edb96de395063e025475f6e9a6ba55ac990f
TheCrimsonHeart1 <tremain255@gmail.com>
2026-01-07 22:52:52 -0500

    Added polish, and a jump

--- Scripts/Entities/Player/Movement/PlayerMovement.gd
+++ Scripts/Entities/Player/Movement/PlayerMovement.gd
@@ -1,14 +1,19 @@
 extends CharacterBody3D
 
-@export var movementSpeed = 10
-@export var accelerationRate = 7
+@export var movementSpeed = 4
+@export var defaultAccelerationRate = 7
 @export var mouseSensitivity = 0.15
-@export var gravityRate = 1
-@export var jumpForce = 10
+@export var gravityRate = 8
+@export var jumpForce = 3
+@export var accelerationRateInAir = 3
+@export var headbobFrequency = 3.0
+@export var headbobAmplitude = 0.04
 
-@onready var playerCamera = $PlayerCamera
+@onready var playerCamera = $PlayerHead/PlayerCamera
 
-var pitch := 0.0
+var accelerationRate = 0
+var pitch = 0.0
+var headbobTime = 0.0
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -22,6 +27,12 @@ func _input(event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	handleMovement(delta)
+	applyGravity(delta)
+	handleJump()
+	
+	if is_on_floor():
+		headbobTime += delta * velocity.length() * float(is_on_floor())
+		playerCamera.transform.origin = headbob(headbobTime)
 	
 func handleMovement(delta):
 	var inputDirection := Vector2(
@@ -37,3 +48,24 @@ func handleMovement(delta):
 	velocity.z = lerp(velocity.z, targetVelocity.z, accelerationRate * delta)
 	
 	move_and_slide()
+
+func applyGravity(delta):
+	if not is_on_floor():
+		accelerationRate = accelerationRateInAir
+		velocity.y -= gravityRate * delta
+	else:
+		velocity.y = 0
+		accelerationRate = defaultAccelerationRate
+
+func handleJump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jumpForce
+		
+
+func headbob(headbobTime):
+	var headbobPosition = Vector3.ZERO
+	headbobPosition.y = sin(headbobTime * headbobFrequency) * headbobAmplitude
+	headbobPosition.x = sin(headbobTime * headbobFrequency / 2) * headbobAmplitude
+	return headbobPosition
+	
+	
